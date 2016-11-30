82e017940a425b756956f58d55b7095f5e8e8315
Rashik Hasnat <rashikhasnat@ymail.com>
2016-12-01 01:04:44 +0600

    Halfway to spellCasting
    Halfway to spellCasting

--- Fantastic Beats/main.cpp
+++ Fantastic Beats/main.cpp
@@ -4,7 +4,15 @@
 ///LinkedIn:bd.linkedin.com/in/rashikhasnat
 ///Khulna University of Engineering & Technology (KUET), Khulna
 ///Bangladesh
-#include <bits/stdc++.h>
+//#include "stdafx.h"
+#include <iostream>
+#include<map>
+#include<vector>
+#include<string>
+#include<algorithm>
+#include<math.h>
+#include<stdio.h>
+
 #define mp make_pair
 #define pb push_back
 #define pii pair<int,int>
@@ -22,66 +30,66 @@
 using namespace std;
 
 /// ******************************** ///
-
 bool isLinier(int a, int b, int m, int n, int x, int y)
 {
-    int t1,t2;
-    t1=(n-b)*(x-m);
-    t2=(y-n)*(m-a);
-    if(t1==t2)
-        return true;
-    else
-        return false;
+	int t1, t2;
+	t1 = (n - b)*(x - m);
+	t2 = (y - n)*(m - a);
+	if (t1 == t2)
+		return true;
+	else
+		return false;
 }
 
 
-struct line{//ax+by=c
-    void values(double x, double y, double z){a=x,b=y,c=z;}
-    double a,b,c;};
+struct line {//ax+by=c
+	void values(double x, double y, double z) { a = x, b = y, c = z; }
+	double a, b, c;
+};
 
-pair<double,double> sectionPoint(line l1, line l2)//ax+by=c
+pair<double, double> sectionPoint(line l1, line l2)//ax+by=c
 {
-    double a1,b1,c1,a2,b2,c2,x,y,dx,dy,d;
+	double a1, b1, c1, a2, b2, c2, x, y, dx, dy, d;
 
-    a1=l1.a,
-    b1=l1.b;
-    c1=l1.c;
-    a2=l2.a;
-    b2=l2.b;
-    c2=l2.c;
-    d=a1*b2-b1*a2;
-    if(d==0)
-        return mp(-1, -1);
-    dx=c1*b2-c2*b1;
-    dy=a1*c2-a2*c1;
-    x=dx*1.0/d;
-    y=dy*1.0/d;
-    cout<<x<<endl<<y<<endl;
-    return mp(x,y);
+	a1 = l1.a,
+		b1 = l1.b;
+	c1 = l1.c;
+	a2 = l2.a;
+	b2 = l2.b;
+	c2 = l2.c;
+	d = a1*b2 - b1*a2;
+	if (d == 0)
+		return mp(-1, -1);
+	dx = c1*b2 - c2*b1;
+	dy = a1*c2 - a2*c1;
+	x = dx*1.0 / d;
+	y = dy*1.0 / d;
+	cout << x << endl << y << endl;
+	return mp(x, y);
 }
 
-line equation(pair<double,double> point1, pair<double,double> point2)
+line equation(pair<double, double> point1, pair<double, double> point2)
 {
-    double x1,x2,y1,y2;
-    x1=point1.first;
-    y1=point1.second;
-    x2=point2.first;
-    y2=point2.second;
-    double kx,ky;
-    line ret;
-    kx=x1-x2;
-    ky=y1-y2;
-    ret.a=ky;
-    ret.b=kx;
-    ret.c=x1*ky-y1*kx;
-    if(ret.a<0)
-    {
-        ret.a*=-1;
-        ret.b*=-1;
-        ret.c*=-1;
-    }
-    cout<<ret.a<<"x+"<<ret.b<<"y="<<ret.c<<endl;
-    return ret;
+	double x1, x2, y1, y2;
+	x1 = point1.first;
+	y1 = point1.second;
+	x2 = point2.first;
+	y2 = point2.second;
+	double kx, ky;
+	line ret;
+	kx = x1 - x2;
+	ky = y1 - y2;
+	ret.a = ky;
+	ret.b = kx;
+	ret.c = x1*ky - y1*kx;
+	if (ret.a<0)
+	{
+		ret.a *= -1;
+		ret.b *= -1;
+		ret.c *= -1;
+	}
+	cout << ret.a << "x+" << ret.b << "y=" << ret.c << endl;
+	return ret;
 }
 
 
@@ -89,10 +97,18 @@ line equation(pair<double,double> point1, pair<double,double> point2)
 double const eps = 1e-9;
 ll manhattan(ll x1, ll y1, ll x2, ll y2)
 {
-    return abs(x1-x2)+abs(y1-y2);
+	return abs(x1 - x2) + abs(y1 - y2);
 }
 
+ll sqrDistance(ll x1, ll y1, ll x2, ll y2)
+{
+	return (x1 - x2)*(x1 - x2) + (y1 - y2)*(y1 - y2);
+}
 
+ll sqrDistance(pii x1, pii x2)
+{
+	return sqrDistance(x1.first, x1.second, x2.first, x2.second);
+}
 
 
 ///************main code starts from below******************///
@@ -103,138 +119,176 @@ int snafle[20];
 int positionDetails[15][6];
 int entities;
 int highSnaffle;
-int const WIZARDRADIUS=400;
-int const SNAFFLERADIUS=150;
+int startIndex, opponentIndex;
+int const WIZARDRADIUS = 400;
+int const SNAFFLERADIUS = 150;
+int const bludgerDanger = 1000;
+int const goalDanger = 1600;
+int const goalNear = 3200;
+int magicPower = -1;
 bool isHolding[15];
 bool isOnField[15];
-int myPlayerCount=0, oponentPlayerCount=0, snafleCount=0;
-pii targetGoalBar;
+int myPlayerCount = 0, oponentPlayerCount = 0, snafleCount = 0;
+pii targetGoalBar, myGoalBar;
+						///0,			1,			  2,		3
+string magicSpells[] = {"OBLIVIATE", "PETRIFICUS", "ACCIO", "FLIPENDO"};
+int magiCost[] = { 5,10, 20, 20 };
+
+
 ///myPlayer 0, oponnent 1, snifle 2;
 int getEntityTypeInt(string entityType)
 {
-    if(entityType=="WIZARD")
-        return 0;
-    if(entityType=="OPPONENT_WIZARD")
-        return 1;
-    if(entityType=="SNAFFLE")
-        return 2;
+	if (entityType == "WIZARD")
+		return 0;
+	if (entityType == "OPPONENT_WIZARD")
+		return 1;
+	if (entityType == "SNAFFLE")
+		return 2;
 }
 int getNearestSnaffle(int wizardIndex)
 {
-    int resultIndex,nowDist,playerX,playerY, targetX, targetY;
-    playerX=positionDetails[wizardIndex][1];
-    playerY=positionDetails[wizardIndex][2];
-    int mn=1<<30;
-    cerr<<"Nearest search for wizard "<<wizardIndex<<' '<<playerX<<' '<<playerY<<":\n";
-    for(int i=4; i<=highSnaffle; i++)
-    {
-        cerr<<"GOT HERE "<<i<<endl;
-        if(!isOnField[i])
-            continue;
-        cerr<<"passed it!! "<<i<<endl;
-        targetX=positionDetails[i][1];
-        targetY=positionDetails[i][2];
-        nowDist=manhattan(targetX,targetY,playerX, playerY);
-        cerr<<'\t'<<i<<" "<<targetX<<' '<<targetY<<' '<<nowDist<<endl;
-        if(nowDist<mn)
-        {
-            mn=nowDist;
-            resultIndex=i;
-        }
-    }
-    cerr<<endl<<endl;
-    return resultIndex;
+	int resultIndex, nowDist, playerX, playerY, targetX, targetY;
+	playerX = positionDetails[wizardIndex][1];
+	playerY = positionDetails[wizardIndex][2];
+	ll mn = (ll)1 << 60;
+	cerr << "Nearest search for wizard " << wizardIndex << ' ' << playerX << ' ' << playerY << ":\n";
+	for (int i = 4; i <= highSnaffle; i++)
+	{
+		cerr << "GOT HERE " << i << endl;
+		if (!isOnField[i])
+			continue;
+		cerr << "passed it!! " << i << endl;
+		targetX = positionDetails[i][1];
+		targetY = positionDetails[i][2];
+		nowDist = sqrDistance(targetX, targetY, playerX, playerY);
+		cerr << '\t' << i << " " << targetX << ' ' << targetY << ' ' << nowDist << endl;
+		if (nowDist<mn)
+		{
+			mn = nowDist;
+			resultIndex = i;
+		}
+	}
+	cerr << endl << endl;
+	return resultIndex;
 }
 
 pii getTargetGoalBar(int playerIndex)
 {
-    if(playerIndex==1)
-    {
-        return mp(0,3750);
-    }
-    else
-        return mp(16000,3750);
+	if (playerIndex == 1)
+	{
+		myGoalBar = mp(16000, 3750);
+		return mp(0, 3750);
+	}
+	myGoalBar = mp(0, 3750);
+	return mp(16000, 3750);
 }
 
-//void throwSniffle(int playerIndex, int sniffleIndex)
-//{
-//
-//}
+
+bool isDefender(int currentPlayerIndex, int otherPlayerIndex)
+{
+	ll currentPlayerDistance = sqrDistance(positionDetails[currentPlayerIndex][1], positionDetails[currentPlayerIndex][2], targetGoalBar.first, targetGoalBar.second);
+	ll otherPlayerDistance = sqrDistance(positionDetails[otherPlayerIndex][1], positionDetails[otherPlayerIndex][2], targetGoalBar.first, targetGoalBar.second);
+	if (currentPlayerDistance > otherPlayerDistance)
+		return true;
+	return false;
+}
+void castSpell(int spellIndex, int entityId)
+{
+	cout << magicSpells[spellIndex] << ' ' << entityId << endl;
+	magicPower -= magiCost[spellIndex];
+}
+bool canCastSpell(int spellIndex)
+{
+	return magicPower >= magiCost[spellIndex];
+}
+void PlayDefensive(int playerIndex)
+{
+	pii myPosition = mp(positionDetails[playerIndex][1], positionDetails[playerIndex][2]);
+	for (int i = opponentIndex; i <= opponentIndex + 1; i++)
+	{
+		pii opponentPosition = mp(positionDetails[i][1], positionDetails[i][2]);
+		//if(sqrDistance(myGoalBar, opponentPosition)<sqrDistance(myGoalBar, myPosition) && )
+	}
+}
+
 
 int main()
 {
-    int myTeamId; // if 0 you need to score on the right of the map, if 1 you need to score on the left
-    cin >> myTeamId; cin.ignore();
-    targetGoalBar=getTargetGoalBar(myTeamId);
-    // game loop
-    while (1) {
-         // number of entities still in game
-//        mem(grid, -1);
-        mem(isHolding,false);
-        mem(isOnField, false);
-        highSnaffle=0;
-        cin >> entities; cin.ignore();
-        myPlayerCount=0, oponentPlayerCount=0, snafleCount=0;
-        for (int i = 0; i < entities; i++) {
-            int entityId; // entity identifier
-            string entityType; // "WIZARD", "OPPONENT_WIZARD" or "SNAFFLE" (or "BLUDGER" after first league)
-            int x; // position
-            int y; // position
-            int vx; // velocity
-            int vy; // velocity
-            int state; // 1 if the wizard is holding a Snaffle, 0 otherwise
-            int entityTypeInt;
-            cin >> entityId >> entityType ;
-            entityTypeInt=getEntityTypeInt(entityType);
-            positionDetails[entityId][0]=entityTypeInt;
-            for(int j=1; j<=5; j++)
-            {
-                cin>> positionDetails[entityId][j];
-            }
-            isOnField[entityId]=true;
-            isHolding[entityId]=positionDetails[entityId][5];
-            if(entityTypeInt==2)
-            {
-                snafleCount++;
-                highSnaffle=max(highSnaffle,entityId);
-            }
-        }
+	int myTeamId; // if 0 you need to score on the right of the map, if 1 you need to score on the left
+	cin >> myTeamId; cin.ignore();
+	targetGoalBar = getTargetGoalBar(myTeamId);
+	if (myTeamId == 1)
+	{
+		startIndex = 2;
+		opponentIndex = 0;
+	}
+	else
+	{
+		startIndex = 0;
+		opponentIndex = 2;
 
-        int startIndex,nowIndex;
-        if(myTeamId==1)
-            startIndex=2;
-        else
-            startIndex=0;
-        for (int i = 0; i < 2; i++) {
+	}
+	// game loop
+	while (1) {
+		magicPower++;
+		mem(isHolding, false);
+		mem(isOnField, false);
+		highSnaffle = 0;
+		cin >> entities; cin.ignore();
+		myPlayerCount = 0, oponentPlayerCount = 0, snafleCount = 0;
+		for (int i = 0; i < entities; i++) {
+			int entityId; // entity identifier
+			string entityType; // "WIZARD", "OPPONENT_WIZARD" or "SNAFFLE" (or "BLUDGER" after first league)
+			int x; // position
+			int y; // position
+			int vx; // velocity
+			int vy; // velocity
+			int state; // 1 if the wizard is holding a Snaffle, 0 otherwise
+			int entityTypeInt;
+			cin >> entityId >> entityType;
+			entityTypeInt = getEntityTypeInt(entityType);
+			positionDetails[entityId][0] = entityTypeInt;
+			for (int j = 1; j <= 5; j++)
+			{
+				cin >> positionDetails[entityId][j];
+			}
+			isOnField[entityId] = true;
+			isHolding[entityId] = positionDetails[entityId][5];
+			if (entityTypeInt == 2)
+			{
+				snafleCount++;
+				highSnaffle = max(highSnaffle, entityId);
+			}
+		}
+		int nowIndex;
 
-            // Write an action using cout. DON'T FORGET THE "<< endl"
-            // To debug: cerr << "Debug messages..." << endl;
+		for (int i = 0; i < 2; i++) {
+			nowIndex = i + startIndex;
+			//for (int i = 0; i <= 10; i++)
+			//{
+			//	cerr << i << ' ' << isHolding[i] << endl;
+			//}
+			if (isHolding[nowIndex])
+			{
+				cout << "THROW " << targetGoalBar.first << " " << targetGoalBar.second << " 500\n";
+			}
+			else
+			{
+				if (isDefender(nowIndex, startIndex + (i + 1) % 2))
+				{
+					PlayDefensive(nowIndex);
+				}
+				int targetedSnaffle = getNearestSnaffle(nowIndex);
+				if (snafleCount != 1)
+				{
+					isOnField[targetedSnaffle] = false;
+				}
+				// cerr<<targetedSnaffle<<endl;
+				cout << "MOVE " << positionDetails[targetedSnaffle][1] << " " << positionDetails[targetedSnaffle][2] << " 150\n";
+			}
 
-
-            // Edit this line to indicate the action for each wizard (0 ≤ thrust ≤ 150, 0 ≤ power ≤ 500)
-            // i.e.: "MOVE x y thrust" or "THROW x y power"
-            nowIndex=i+startIndex;
-            for(int i=0;i <=10; i++)
-            {
-                cerr<<i<<' '<<isHolding[i]<<endl;
-            }
-            if(isHolding[nowIndex])
-            {
-                cout<<"THROW "<<targetGoalBar.first<<" "<<targetGoalBar.second<<" 500\n";
-            }
-            else
-            {
-                int targetedSnaffle=getNearestSnaffle(nowIndex);
-                if(snafleCount!=1)
-                {
-                    isOnField[targetedSnaffle]=false;
-                }
-               // cerr<<targetedSnaffle<<endl;
-                cout << "MOVE " << positionDetails[targetedSnaffle][1]<<" "<<positionDetails[targetedSnaffle][2]<<" 150\n";
-            }
-
-        }
-    }
+		}
+	}
 }
 
 
